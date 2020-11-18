4fc130c71bc20fb0a4eeb3c682481425acabf29d
ahorvath12 <ahorvath@scu.edu>
2020-11-18 13:49:58 -0800

    added hands & tools

---
+++ Assets/Scripts/DemonController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemonController : MonoBehaviour
+{
+    public GameObject mesh1, mesh2;
+    public bool walk, crouch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (walk)
+        {
+            GetComponent<Animator>().SetTrigger("Walk");
+        }
+        else if(crouch)
+        {
+            GetComponent<Animator>().SetTrigger("Crouch");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Player enter");
+            GetComponent<Animator>().SetTrigger("Fade");
+        }
+    }
+}
