d72a49cac0f38074758faaf1c1daa684616eff1c
ahorvath12 <ahorvath@scu.edu>
2020-11-19 14:06:14 -0800

    added night vision camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject child;
+
+    private Camera cam;
+    private Animator anim;
+
+    bool raising;
+    float timeCheck=0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = child.GetComponent<Camera>();
+        cam.enabled = false;
+
+        anim = GetComponent<Animator>();
+
+        if (gameObject.name != "HandCamera")
+            GetComponent<CameraController>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (cam.enabled)
+                cam.enabled = false;
+        }
+        
+    }
+
+    public void TurnOn()
+    {
+        if (gameObject.name == "HandCamera")
+            cam.enabled = true;
+    }
+    
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator[] hands;
+
+    private int index = 0;
+    private bool isPlaying;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hands[index].SetTrigger("Raise");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlaying && !hands[index].GetCurrentAnimatorStateInfo(0).IsName("Raise"))
+        {
+            isPlaying = false;
+            index++;
+            if (index >= hands.Length)
+                index = 0;
+            
+            hands[index].SetTrigger("Raise");
+            
+        }
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Tab))
+        {
+            hands[index].SetTrigger("Lower");
+            isPlaying = true;
+        }
+
+        
+    }
+}
